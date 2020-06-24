ca451bdfd8a0ddf18c7d4b419e5059a550c0abae
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-06-24 16:26:48 +0400

    Initial commit with Quote model

---
+++ src/test/java/guru/springframework/webfluxqservice/WebfluxStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.webfluxqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
